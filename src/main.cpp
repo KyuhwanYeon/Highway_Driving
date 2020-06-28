3fae3ea12498b2c528e0b4734e12a9375aaca71f
KyuhwanYeon <KyuhwanYeon@gmail.com>
2020-06-28 20:48:42 +0900

    Add spline path generation

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "helpers.h"
 #include "json.hpp"
 #include "lpp.h"
+#include "spline.h"
 // for convenience
 using nlohmann::json;
 using std::string;
@@ -95,33 +96,127 @@ int main()
 
           json msgJson;
 
-          vector<double> next_x_vals;
-          vector<double> next_y_vals;
-
           /**
            * TODO: Spline path planning generation 
            */
+          int lane = 1;
+          int next_wp = -1;
+          double ref_vel = 49.5; //mph
+          int prev_size = previous_path_x.size();
+          vector<double> ptsx;
+          vector<double> ptsy;
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
 
-          
-          double dist_inc = 0.1;
-          vector<double> start = {1,2,3};
-          vector<double> end = {2,3,4};
-          double Time = 3;
-          vector<double> coeff = JMT(start, end, Time);
-          for (int i = 0; i < 50; ++i)
+          // ptsx, ptsy are for spline
+          // ptsx[0],ptsx[1] are for tangent of previous path
+          if (prev_size < 2)
           {
-            double next_s = car_s + (i+1)*dist_inc;
-            double next_d = 6;
-             
-            vector<double> xy = getXY(next_s, next_d, map_waypoints_s,map_waypoints_x,map_waypoints_y);
-            //printf("car_x: %lf, car_y: %lf, car_s: %lf, next s: %lf, next_x: %lf, next_y: %lf \n", car_x, car_y, car_s, next_s,xy[0],xy[1]);
-            next_x_vals.push_back(xy[0]);
-            next_y_vals.push_back(xy[1]);
-            
+            double prev_car_x = car_x - cos(car_yaw);
+            double prev_car_y = car_y - sin(car_yaw);
+
+            ptsx.push_back(prev_car_x);
+            ptsx.push_back(car_x);
+
+            ptsy.push_back(prev_car_y);
+            ptsy.push_back(car_y);
           }
-          int path_size = previous_path_x.size();
+          else
+          {
+            ref_x = previous_path_x[prev_size - 1];
+            ref_y = previous_path_y[prev_size - 1];
+            double ref_x_prev = previous_path_x[prev_size - 2];
+            double ref_y_prev = previous_path_y[prev_size - 2];
+            ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+
+            ptsx.push_back(previous_path_x[prev_size - 2]);
+            ptsx.push_back(previous_path_x[prev_size - 1]);
+            ptsy.push_back(previous_path_y[prev_size - 2]);
+            ptsy.push_back(previous_path_y[prev_size - 1]);
+            car_speed = (sqrt((ref_x - ref_x_prev) * (ref_x - ref_x_prev) + (ref_y - ref_y_prev) * (ref_y - ref_y_prev)) / .02) * 2.237;
+          }
+          // ptsx[2],[3],[4] are netx wp goal
+          vector<double> next_wp0 = getXY(car_s + 30, (2 + 4 * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp1 = getXY(car_s + 60, (2 + 4 * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp2 = getXY(car_s + 90, (2 + 4 * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
+
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);
+
+          // local coorinate, after shift, ref_x and ref_y is coordinate (0, 0)
+          for (int i = 0; i < ptsx.size(); i++)
+          {
+            printf("previous ptsx[i], ptsy[i]: %lf, %lf \n", ptsx[i], ptsy[i]);
+            printf("ref_x, ref_y : %lf, %lf \n", ref_x,ref_y);
+
+            //shift car reference angle to 0 degrees
+            double shift_x = ptsx[i] - ref_x;
+            double shift_y = ptsy[i] - ref_y;
+            printf("shift_x, shift_y : %lf, %lf \n", shift_x,shift_y);
+
+
+            ptsx[i] = (shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw));
+            ptsy[i] = (shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw));
+            printf("after ptsx[i], ptsy[i]: %lf, %lf \n\n", ptsx[i], ptsy[i]);
+          }
+          printf("---------------------------------\n");
           
-          printf("previous_pathsize: %d, next x: %lf, next_y: %lf \n\n", path_size, next_x_vals[0], next_y_vals[0]);
+          // spline fitting
+          tk::spline s;
+
+          s.set_points(ptsx, ptsy);
+
+          vector<double> next_x_vals;
+          vector<double> next_y_vals;
+
+          for (int i = 0; i < previous_path_x.size(); i++)
+          {
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
+
+          double target_x = 30.0;
+          double target_y = s(target_x);
+          double target_dist = sqrt((target_x) * (target_x) + (target_y) * (target_y));
+
+          double x_add_on = 0;
+
+          for (int i = 1; i <= 50 - previous_path_x.size(); i++)
+          {
+
+            if (ref_vel > car_speed)
+            {
+              car_speed += .224;
+            }
+            else if (ref_vel < car_speed)
+            {
+              car_speed -= .224;
+            }
+
+            double N = (target_dist / (.02 * car_speed / 2.24));
+            double x_point = x_add_on + (target_x) / N;
+            double y_point = s(x_point);
+
+            x_add_on = x_point;
+
+            double x_ref = x_point;
+            double y_ref = y_point;
+
+            x_point = (x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw));
+            y_point = (x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw));
+
+            x_point += ref_x;
+            y_point += ref_y;
+
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
+          }
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
