b32075036c82ded99e2b0961aeb79d7522789c38
kyuhwan.yeon <kyuhwanyeon@gmail.com>
2020-07-11 19:28:46 +0900

    Add vehicle class

---
+++ src/vehicle/vehicle.h
@@ -0,0 +1,23 @@
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+#include <cmath>
+#include <iostream>
+#include "../util/Eigen-3.3/Eigen/Dense"
+#include <vector>
+#include "../util/helpers.h"
+#include "../util/json.hpp"
+using nlohmann::json;
+using std::vector;
+
+class Vehicle
+{
+    // constant acceleration vehicle
+public:
+    Vehicle();
+    Vehicle(int lane, float s, float v, float a, string state = "CS");
+
+private:
+};
+
+#endif
